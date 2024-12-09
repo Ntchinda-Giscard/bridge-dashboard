4c21c20deca5d0ca51bcceb60e2e89be2ad7eca5
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-12-09 02:44:33 +0100

    first commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,63 @@
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+export default function ResponsiveSizes({children,}: Readonly<{ children: React.ReactNode; }>
+) {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: { base: 60, md: 70, lg: 80 } }}
+      navbar={{
+        width: { base: 200, md: 300, lg: 400 },
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
+
+
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+// import '@mantine/core/styles.css';
+
+// import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+
+// export const metadata = {
+//   title: 'My Mantine app',
+//   description: 'I have followed setup instructions carefully',
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <ColorSchemeScript />
+//       </head>
+//       <body>
+//         <MantineProvider>{children}</MantineProvider>
+//       </body>
+//     </html>
+//   );
+// }
