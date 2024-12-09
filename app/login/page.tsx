d623e49478b1726975a7218e3e554984608907b2
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-12-09 03:07:14 +0100

    dashboard page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,9 +12,9 @@ import {
   
   export function AuthenticationImage() {
     return (
-      <div className={classes.wrapper}>
-        <Paper className={classes.form} radius={0} p={30}>
-          <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
+      <div className={''}>
+        <Paper className={''} radius={0} p={30}>
+          <Title order={2} className={''} ta="center" mt="md" mb={50}>
             Welcome back to Mantine!
           </Title>
   
