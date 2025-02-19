d64d3e405e7c07a17a2d0123d7e54227d6d7e4f9
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-02-19 18:41:19 +0100

    naled stance

--- app/dashboard/demands/components/addServiceRequest.tsx
+++ app/dashboard/demands/components/addServiceRequest.tsx
@@ -7,11 +7,13 @@ import axios from "axios";
 import toast from 'react-hot-toast';
 import { CLIENT_USER } from '../query/getClientUser';
 import { GET_SERVICES } from '../query/getService';
+import { INSERT_DEMANDE } from '../mutation/mutation';
 
 export default function AddDemand({opened, close}: any) {
 
   const {data: dataClient, error: errClients, loading: loadClients} = useQuery(CLIENT_USER);
   const {data: dataService, error: errService, loading: loadService} = useQuery(GET_SERVICES);
+  const [insertDemande, {loading: loadInsertDemande}] = useMutation(INSERT_DEMANDE);
     
     const form = useForm({
         mode: 'uncontrolled',
@@ -33,7 +35,21 @@ export default function AddDemand({opened, close}: any) {
 
  
     function handleSubmit(values: any){
-        console.log(values)
+        console.log(values);
+        insertDemande({
+          variables:{
+            client_id: values?.clients,
+            service_id: values?.service
+          },
+          onCompleted: () =>{
+            toast.success("Operation successful")
+            close();
+            form.reset();
+          },
+          onError: () =>{
+            toast.error("Operation failed")
+          }
+        });
 
     }
 
@@ -52,7 +68,7 @@ export default function AddDemand({opened, close}: any) {
         setServices(dataServices)
         setClients(dataClients)
 
-    }, []);
+    }, [dataService, dataClient]);
 
   return (
     <>
@@ -60,39 +76,39 @@ export default function AddDemand({opened, close}: any) {
         {/* Modal content */}
         <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
 
-        <Select
-                        mt={'lg'}
-                        withAsterisk
-                        radius={'sm'}
-                        data={clients}
-                        label="Genre"
-                        placeholder="Clients"
-                        searchable
-                        key={form.key('clients')}
-                        {...form.getInputProps('clients')}
-                        styles={{
-                            label:{color: "#404040"},
-                            option:{color: "#404040"}
-                        }}
-                    />
-                    <Select
-                        mt={'lg'}
-                        withAsterisk
-                        radius={'sm'}
-                        data={services}
-                        label="Role"
-                        placeholder="select"
-                        searchable
-                        key={form.key('service')}
-                        {...form.getInputProps('service')}
-                        styles={{
-                            label:{color: "#404040"},
-                            option:{color: "#404040"}
-                        }}
-                    />
+          <Select
+            mt={'lg'}
+            withAsterisk
+            radius={'sm'}
+            data={clients}
+            label="Clients"
+            placeholder="Clients"
+            searchable
+            key={form.key('clients')}
+            {...form.getInputProps('clients')}
+            styles={{
+                label:{color: "#404040"},
+                option:{color: "#404040"}
+            }}
+          />
+          <Select
+            mt={'lg'}
+            withAsterisk
+            radius={'sm'}
+            data={services}
+            label="Service"
+            placeholder="select"
+            searchable
+            key={form.key('service')}
+            {...form.getInputProps('service')}
+            styles={{
+                label:{color: "#404040"},
+                option:{color: "#404040"}
+            }}
+          />
 
             <Group justify="flex-end" mt="md" grow>
-                <Button bg={"#0B8F23"} type="submit">Soumettre</Button>
+                <Button bg={"#0B8F23"} loading={loadInsertDemande} type="submit">Soumettre</Button>
                 <Button color={"#0B8F23"} variant='outline' onClick={close} >Annuler</Button>
             </Group>
         </form>
