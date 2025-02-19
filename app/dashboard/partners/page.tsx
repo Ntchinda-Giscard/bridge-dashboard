be3263699dac6bc5de8bf28fab1099c5a2de3c9a
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-02-19 19:52:04 +0100

    lowa

---
+++ app/dashboard/partners/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { Paper } from '@mantine/core'
+
+function Page() {
+    return ( <>
+        <main className="flex min-h-full flex-col gap-3">
+            <p style={{fontWeight: 800, fontSize: "large", color: "#404040"}}> Gestion partainer </p>
+            <Paper radius='md' shadow='md'>
+
+            </Paper>
+        </main>
+    </> );
+}
+
+export default Page;
